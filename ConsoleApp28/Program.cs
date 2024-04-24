4b03cc51b12839af468941554027d5a4f746e38a
Aytac <111279845+Seniorita97@users.noreply.github.com>
2024-04-24 08:44:05 +0400

    Task Finished

---
+++ ConsoleApp28/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp28
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Student ali = new Student();
+            Console.WriteLine(ali.Age);
+
+            Student murad = new Student();
+            Console.WriteLine(murad.Age);
+
+            Student gadir = new Student();
+            Console.WriteLine(gadir.Age);
+
+            Student nicat = new Student();
+            Console.WriteLine(nicat.Age);
+        }
+
+        public class Student
+        {
+            private static int lastAge = 0;
+            public int Age { get; }
+
+            public Student()
+            {
+                lastAge++;
+                Age = lastAge;
+            }
+        }
+    }
+}
